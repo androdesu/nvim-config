79813b8acefedb481ae04d1cc240223a3b7860a3
androdesu <moulayyoussef.eladlouni@gmail.com>
2025-12-14 13:08:05 -0500

    changes

---
+++ lua/sethy/plugins/nvim-dap.lua
@@ -0,0 +1,24 @@
+local dap = require("dap")
+
+dap.adapters.codelldb = {
+	type = "server",
+	port = "${port}",
+	executable = {
+		-- Change this to your path!
+		command = "/opt/codelldb/adapter/codelldb",
+		args = { "--port", "${port}" },
+	},
+}
+
+dap.configurations.rust = {
+	{
+		name = "Launch file",
+		type = "codelldb",
+		request = "launch",
+		program = function()
+			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+		end,
+		cwd = "${workspaceFolder}",
+		stopOnEntry = false,
+	},
+}
